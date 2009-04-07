77dac6b84b39e9dea6bc18e407699a44da893d94
Daniel Soto <danielrsoto@gmail.com>
2009-04-07 12:03:40 -0700

    changes to plotting code

--- adhesion/random_surface.py
+++ adhesion/random_surface.py
@@ -30,13 +30,22 @@ x = np.linspace(0,2*3.14,5000)
 y = np.zeros((N+1,len(x)))
 # offset surface from zero
 y = y + 2
+# initialize energy arrays
+surfaceEnergy = np.zeros((N+1,len(x)))
+deformationEnergy = np.zeros((N+1,len(x)))
+totalEnergy = np.zeros((N+1,len(x)))
 
-# create surface for each of i frequency components
 for i in range(1,N+1):
+    # create surface for each of i frequency components
     if i//2 == 1:
         y[i] = y[i-1] + 1/(i+1)**p*np.sin((i+1)*x + 2*3.14*phase[i-1])
     else:
         y[i] = y[i-1] - 1/(i+1)**p*np.sin((i+1)*x + 2*3.14*phase[i-1])
+
+    # create plot for energy balance
+    surfaceEnergy[i] = -0.5 * x
+    deformationEnergy[i] = 0.5 * (N+1-i) / (N+1) * x**2
+    totalEnergy[i] = surfaceEnergy[i] + deformationEnergy[i]
 
 # generate plots
 for i in range(N+1):
@@ -44,11 +53,20 @@ for i in range(N+1):
     print 'generating figure ' + fileName
     thisPlotSep = plotSep - (plotSep - 0.5) * i / N
     fig = mpl.figure()
-    fig.set_size_inches((8,1.5))
-    ax = fig.add_subplot(111)
+    fig.set_size_inches((6,4))
+    # surface plot
+    ax = fig.add_subplot(211)
     ax.fill_between(x, y[i]+thisPlotSep, plotTop, color = 'b')
     ax.fill_between(x, y[N-1], 0, color = 'r')
     ax.axis([0, 6, 0, plotTop])
     ax.set_axis_off()
+    # energy plot
+    ax = fig.add_subplot(212)
+    ax.plot(x, surfaceEnergy[i])
+    ax.plot(x, deformationEnergy[i])
+    ax.plot(x, totalEnergy[i])
+    ax.axis([0, 6, -1, 1])
+    ax.set_xlabel('Intimate Contact (arb units)')
+    ax.set_ylabel('Energy (arb units)')
     fig.savefig(fileName)
     mpl.close()
