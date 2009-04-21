dc3032449232c1eea2da8d018a29f02ce2a9a604
Daniel Soto <danielrsoto@gmail.com>
2009-04-21 09:12:21 -0700

    added legend and cross hair for min energy

--- adhesion/random_surface.py
+++ adhesion/random_surface.py
@@ -10,8 +10,6 @@ import numpy.random      as nr
 import matplotlib.pyplot as mpl
 import os
 
-# seed random number generator
-
 # number of frequency components
 N = 50
 # scaling exponent
@@ -21,8 +19,9 @@ plotTop = 6
 # plot separation between surfaces
 plotSep = 2
 
-# create vector of random phases
+# seed random number generator
 nr.seed(1)
+# create vector of random phases
 phase = nr.rand(N)
 
 # initialize data arrays
@@ -31,12 +30,13 @@ y = np.zeros((N+1,len(x)))
 # offset surface from zero
 y = y + 2
 # initialize energy arrays
-surfaceEnergy = np.zeros((N+1,len(x)))
+surfaceEnergy     = np.zeros((N+1,len(x)))
 deformationEnergy = np.zeros((N+1,len(x)))
-totalEnergy = np.zeros((N+1,len(x)))
+totalEnergy       = np.zeros((N+1,len(x)))
 
 for i in range(1,N+1):
-    # create surface for each of i frequency components
+    # create surface from each of i frequency components
+    # using series of sines with random phase
     if i//2 == 1:
         y[i] = y[i-1] + 1/(i+1)**p*np.sin((i+1)*x + 2*3.14*phase[i-1])
     else:
@@ -47,6 +47,11 @@ for i in range(1,N+1):
     deformationEnergy[i] = 0.5 * (N+1-i) / (N+1) * x**2
     totalEnergy[i] = surfaceEnergy[i] + deformationEnergy[i]
 
+# create plots for i=0 case
+surfaceEnergy[0] = -0.5 * x
+deformationEnergy[0] = 0.5 * x**2
+totalEnergy[0] = surfaceEnergy[0] + deformationEnergy[0]
+
 # generate plots
 for i in range(N+1):
     fileName = 'plot' + '%02d'%(i) + '.png'
@@ -56,16 +61,30 @@ for i in range(N+1):
     fig.set_size_inches((6,4))
     # surface plot
     ax = fig.add_subplot(211)
+    # deformed adhesive
     ax.fill_between(x, y[i]+thisPlotSep, plotTop, color = 'b')
+    # rough substrate
     ax.fill_between(x, y[N-1], 0, color = 'r')
     ax.axis([0, 6, 0, plotTop])
     ax.set_axis_off()
     # energy plot
     ax = fig.add_subplot(212)
-    ax.plot(x, surfaceEnergy[i])
-    ax.plot(x, deformationEnergy[i])
-    ax.plot(x, totalEnergy[i])
-    ax.axis([0, 6, -1, 1])
+    ax.plot(x, surfaceEnergy[i],     label='Surface Energy')
+    ax.plot(x, deformationEnergy[i], label='Deformation Energy')
+    ax.plot(x, totalEnergy[i],       label='Total Energy')
+    # find min of total energy
+    # plot grey line horizontal and vertical through min
+    energyMinX = x[np.argmin(totalEnergy[i])]
+    energyMinY = np.min(totalEnergy[i])
+    plotMinX = np.linspace(0,6,10)
+    plotMinY = energyMinY * np.ones(10)
+    ax.plot(plotMinX, plotMinY, 'k')
+    plotVertX = energyMinX * np.ones(10)
+    plotVertY = np.linspace(-2,2,10)
+    print plotVertX, plotVertY
+    ax.plot(plotVertX, plotVertY, 'k')
+    ax.legend()
+    ax.axis([0, 6, -2, 2])
     ax.set_xlabel('Intimate Contact (arb units)')
     ax.set_ylabel('Energy (arb units)')
     fig.savefig(fileName)
