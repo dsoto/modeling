6e75e0be2dba3a3f53929ff39853b449eac022cc
Daniel Soto <danielrsoto@gmail.com>
2009-05-12 12:44:09 -0700

    added multiple plots using r_eff = sqrt(r1 * r2) and put plot in thesis

--- jkr/jkr.py
+++ jkr/jkr.py
@@ -8,16 +8,22 @@ import numpy as np
 pi = 3.1415
 gamma = 0.05
 
-R = np.linspace(1,10,11)
+R1 = np.linspace(1,10,11)
+R2 = [10, 20, 30, 40, 50]
 
-adhesion = 3*pi/2*R*gamma
+adhesion = np.zeros((len(R2),len(R1)))
 
 import matplotlib.pyplot as mpl
 
 figure = mpl.figure()
-ax = figure.add_subplot(111)
-ax.plot(R, adhesion, label='adhesion')
-ax.set_xlabel('Radius (um)')
-ax.set_ylabel('Pulloff force (uN)')
-ax.legend()
+axes = figure.add_subplot(111)
+
+for i, val in enumerate(R2):
+    adhesion[i] = 3.0*pi/2.0*(R1*val)**(0.5)*gamma
+    axes.plot(R1, adhesion[i], label=str(val)+r'$\mu$m')
+
+axes.set_xlabel(r'Radius ($\mu$m)')
+axes.set_ylabel(r'Pulloff force ($\mu$N)')
+axes.grid(True,color=((0.5,0.5,0.5)))
+axes.legend()
 mpl.show()
