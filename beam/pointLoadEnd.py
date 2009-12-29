5dae8cf477d5173bcdd27dae87081a2eb311beed
Daniel Soto <danielrsoto@gmail.com>
2009-12-28 16:02:29 -0800

    so, this code can now find the end slope but it does a brute force solution and does not iterate to find the zero

--- beam/pointLoadEnd.py
+++ beam/pointLoadEnd.py
@@ -26,14 +26,14 @@ from scipy           import zeros
 from scipy           import linspace
 from scipy.optimize  import fsolve
 
-E = 200e3           # modulus of pdms
-t = 10e-6           # thickness of beam
-w = 10e-6           # width of beam
+E = 2e6           # modulus of pdms
+t = 19e-6           # thickness of beam
+w = 19e-6           # width of beam
 I = t**3 * w / 12.0 # moment of inertia
-L = 100e-6           # length of beam
+L = 52e-6           # length of beam
 
-debug = True 
-#debug = False
+#debug = True 
+debug = False
 # this function returns the normalized arc length of
 # a beam using the arc length integral
 def arcLengthIntegral(psiL):
@@ -50,8 +50,7 @@ def arcLengthElement(psi,psiL):
     # how do i manage the singularity problem here during the solve routine?
     # if psiL is greater than pi/2, this will always return imaginary
     answer = 1/sqrt(sin(psiL)-sin(psi))
-    # here my janky solution is to only take the real part
-    #answer = answer.real
+    # here my janky solution is to only take the absolute value
     answer = abs(answer)
     if (debug): 
         pass
@@ -72,22 +71,71 @@ def beamLength(psiL,load):
 def solveFunction(psiL, load):
     return L - beamLength(psiL,load)
     
-nPts = 1
+nPts = 5
 loadArray = linspace(.1,20,nPts) * 1e-6     # shear load on beam
-loadArray = linspace(.1,.2,nPts) * 1e-6     # shear load on beam
-loadArray = [.20*1e-6]
+loadArray = linspace(0,1,nPts) * 1e-6     # shear load on beam
+#loadArray = [5000*1e-12]
+#loadArray = [0.1e-6]
 if (debug): print loadArray
 #length = linspace(1,6,nPts)
 
 #'''
 # loop through values
-angleGuess = 1.0
+angleGuess = .2
 angle = zeros(nPts)
+angleTry = linspace(1e-9, 3.1415/2, 200)
 for i, thisLoad in enumerate(loadArray):
-    angle[i] = fsolve(solveFunction, angleGuess, thisLoad)
-    print thisLoad, angle[i]
+    #try brute force solution to avoid problem with imaginary integrand
+    for thisAngle in angleTry:
+        error = L - beamLength(thisAngle, thisLoad)
+        if (error < 0) :
+            if (debug): print 'final angle =',thisAngle
+            break
+        #angle[i] = fsolve(solveFunction, angleGuess, thisLoad)
+    #print thisLoad, angle[i]
     print '-'*50
+    print thisLoad, thisAngle
 #''' 
+
+'''
+at this point i have the final angle solved
+now i can integrate the differential equation to get the 
+form of the beam
+'''
+
+
+
+# December 23, 2009 7:02:25 PM -0800
+# when i come back next time, i need to figure out the ODE solution
+'''
+# derivative of ODE for beam shape
+def derivative(psi, s):
+    return sp.sqrt(sin(psiL)-sin(psi))
+    
+initialCondition = 0.0
+
+nPts = 1000
+mesh = sp.linspace(0,1,nPts)
+
+# solve ode to get slope function
+answer = odeint(derivative, initialCondition, mesh)
+# deal with silly mismatch between mesh dimensions and answer dimensions
+answer = sp.transpose(answer)[0]
+
+# initialize position arrays
+x = sp.zeros(nPts)
+y = sp.zeros(nPts)
+
+# take trig functions for position integrals
+xInt = sp.cos(answer)
+yInt = sp.sin(answer)
+
+# loop through and numerically integrate discrete functions
+for i,val in enumerate(mesh):
+    x[i] = trapz(xInt[:i+1],mesh[:i+1])
+    y[i] = trapz(yInt[:i+1],mesh[:i+1])
+''' 
+
 
 '''
 
